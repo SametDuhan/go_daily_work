9c1aaac43078e217efe256d385e22b6de59f1ed6
SametDuhan <16sametduhan16@gmail.com>
2026-09-20 23:08:41 +0300

    Print variables and message in main function
    Added print statements for variables and a message.

---
+++ first_d.go
@@ -0,0 +1,15 @@
+package main 
+
+import "fmt"
+
+func main(){
+  fmt.Println("Go is working well")
+}
+//This is a comment row
+var name string = "Alia"  // type is string
+var surname = "Atreides"  // type is inferred
+a := 14
+
+fmt.Println(name)
+fmt.Println(surname)
+fmt.Println(a)
