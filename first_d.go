b1ba8a68f299e13e26e4ce8294ebdafbf7f0df23
SametDuhan <16sametduhan16@gmail.com>
2026-09-21 23:55:52 +0300

    Add slice examples and print their properties
    Added examples of slice initialization and properties.

--- first_d.go
+++ first_d.go
@@ -17,4 +17,14 @@ var arr1 = [3]int{1,2,3}
 fmt.Println(name)
 fmt.Println(surname)
 fmt.Println(a)
+
+  myslice1 := []int{}
+  fmt.Println(len(myslice1))
+  fmt.Println(cap(myslice1))
+  fmt.Println(myslice1)
+
+myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+fmt.Println(len(myslice2))
+fmt.Println(cap(myslice2))
+fmt.Println(myslice2)
 }
