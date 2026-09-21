ff16588a3f91f6873fba0a5124bfb3a4a611f42c
SametDuhan <16sametduhan16@gmail.com>
2026-09-21 23:53:28 +0300

    Update first_d.go

--- first_d.go
+++ first_d.go
@@ -1,15 +1,20 @@
 package main 
 
 import "fmt"
-
+const PI = 3.14
 func main(){
   fmt.Println("Go is working well")
-}
+
 //This is a comment row
 var name string = "Alia"  // type is string
 var surname = "Atreides"  // type is inferred
-a := 14
+a := 14 
+
+const CONSTYOE type = value
+var arr1 = [3]int{1,2,3}
+
 
 fmt.Println(name)
 fmt.Println(surname)
 fmt.Println(a)
+}
